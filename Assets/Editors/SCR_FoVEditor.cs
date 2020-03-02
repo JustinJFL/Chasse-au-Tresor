5443c11d73e031a8cca57a1c32c3365499cbb4e6
JustinJFL <52304712+JustinJFL@users.noreply.github.com>
2020-03-02 00:41:40 -0500

    game

---
+++ Assets/Editors/SCR_FoVEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(SCR_FoV))]
+public class SCR_FoVEditor : Editor
+{
+ void OnSceneGUI()
+    {
+        SCR_FoV fov = (SCR_FoV)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius);
+    
+    }
+}
